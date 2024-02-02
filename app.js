be2c668b0707aea892f0952c52c1bbab2706e7fb
Dideroot <mrdideroot@gmail.com>
2024-02-02 20:25:58 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS CLOUD PIPELINE TEST IS SUCCESSFUL!'));
+app.get('/', (req, res) => res.send('AWS CLOUD PIPELINE FINAL TEST IS SUCCESSFUL!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
